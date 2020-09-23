b5717275bedb7e46fdc8db325bb768ac94ebfe4a
Thomas Yang <thyang@nvidia.com>
2020-09-23 00:47:08 -0700

    Adding a solution file on AddStrings

---
+++ AddStrings/src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /*
+     *  "123" + "24" = "147"
+     */
+    public String addStrings (String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        return res.reverse().toString();
+    }
+}
