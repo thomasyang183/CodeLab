a5a46bd8d630011368fc606feaeaea2fda346545
Thomas Yang <thyang@nvidia.com>
2020-09-23 23:12:09 -0700

    Added additional comment

--- AddStrings/src/Solution.java
+++ AddStrings/src/Solution.java
@@ -9,6 +9,7 @@ public class Solution {
         int p1 = num1.length() - 1; //set pointer to loop thru a string from right to left
         int p2 = num2.length() - 1;
         while (p1 >= 0 || p2 >= 0) {
+            //use ternary operator
             //set x1 or x2 to be 0, when either one has p1/p2 < 0
             int x1 = p1 >= 0 ? num1.charAt(p1) - '0': 0; // turn ASCII to actual number
             int x2 = p2 >= 0 ? num2.charAt(p2) - '0': 0;
